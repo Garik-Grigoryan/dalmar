5b4bce6b42170daa512b88e7210ffb0a081b589c
markos-mason <marcusmason89654@gmail.com>
2020-08-07 15:15:09 +0400

    commit

--- store/brands.js
+++ store/brands.js
@@ -18,26 +18,26 @@ export const mutations = {
 
 export const actions = {
   async fetch({commit}) {
-    const brands = await this.$axios.$get('http://127.0.0.1:8000/api/brand/get');
+    const brands = await this.$axios.$get('http://apidavmar.neoteric-software.com/api/brand/get');
     commit('setBrands', brands);
   },
   async getBrand({commit}, [id]){
-    const brand = await this.$axios.$get(`http://127.0.0.1:8000/api/brand/get/${id}`);
+    const brand = await this.$axios.$get(`http://apidavmar.neoteric-software.com/api/brand/get/${id}`);
     commit('setBrand', brand)
   },
   async getBrandFilters({commit}, [id]){
-    const brandFilters = await this.$axios.$get(`http://127.0.0.1:8000/api/product/getFilters/${id}`);
+    const brandFilters = await this.$axios.$get(`http://apidavmar.neoteric-software.com/api/product/getFilters/${id}`);
     commit('setBrandFilters', brandFilters)
   },
 
   async updateBrand({commit}, [id, name, order, image, color]){
-    const brand = await this.$axios.$put(`http://127.0.0.1:8000/api/brand/update/${id}`, {'name': name, 'order': order, 'image': image, 'color': color});
+    const brand = await this.$axios.$put(`http://apidavmar.neoteric-software.com/api/brand/update/${id}`, {'name': name, 'order': order, 'image': image, 'color': color});
   },
   async delete({commit}, [id]){
-    const brand = await this.$axios.$delete(`http://127.0.0.1:8000/api/brand/delete/${id}`);
+    const brand = await this.$axios.$delete(`http://apidavmar.neoteric-software.com/api/brand/delete/${id}`);
   },
   async addBrand(ctx, [name, nameRu, order, image, color]){
-   await this.$axios.$post('http://127.0.0.1:8000/api/brand/add', {'name': name, 'nameRu': nameRu, 'order': order, 'image': image, 'color': color});
+   await this.$axios.$post('http://apidavmar.neoteric-software.com/api/brand/add', {'name': name, 'nameRu': nameRu, 'order': order, 'image': image, 'color': color});
   }
 }
 
