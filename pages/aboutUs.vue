fbfc4fcc75e44195c3e6ec26e1777287d5c5d392
markos-mason <marcusmason89654@gmail.com>
2020-08-07 15:27:18 +0400

    commit

--- pages/aboutUs.vue
+++ pages/aboutUs.vue
@@ -14,12 +14,16 @@
         name: "aboutUs",
         layout: 'product',
         async fetch({route, store}) {
-          await store.dispatch('brands/fetch');
+          // await store.dispatch('brands/fetch');
           // await store.dispatch('wishListAndCart/fetch');
-          await store.dispatch('menus/fetch');
+          // await store.dispatch('menus/fetch');
         },
       async mounted() {
         await this.$store.dispatch('wishListAndCart/fetch');
+        await store.dispatch('brands/fetch');
+
+        await store.dispatch('menus/fetch');
+
       },
     }
 </script>
