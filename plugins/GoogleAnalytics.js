79c595ffa787ccac6041ea9e40249e8ed085af55
Edmon Avetyan <edmonavetyan@gmail.com>
2020-09-03 04:00:08 +0400

    commit

--- plugins/GoogleAnalytics.js
+++ plugins/GoogleAnalytics.js
@@ -3,24 +3,47 @@ import vueAnalytics from 'vue-analytics'
 
 const getGDPR = localStorage.getItem('GDPR:accepted')
 
-if (typeof getGDPR !== 'undefined' && getGDPR === 'true') {
+// if (typeof getGDPR !== 'undefined' && getGDPR === 'true') {
+//   Vue.use(vueAnalytics, {
+//     id: 'UA-177199673-1',
+//     disabled: false,
+//     debug: {
+//       enabled: true,
+//       trace: true,
+//       sendHitTask: true
+//     },
+//   })
+// } else if (typeof getGDPR !== 'undefined' && getGDPR === 'false') {
+//   Vue.use(vueAnalytics, {
+//     id: 'UA-177199673-1',
+//     disabled: true,
+//     debug: {
+//       enabled: true,
+//       trace: true,
+//       sendHitTask: true
+//     }
+//   })
+// }
+export default (ctx) => {
+  const getGDPR = localStorage.getItem("GDPR:accepted");
+
   Vue.use(vueAnalytics, {
-    id: 'UA-177199673-1',
-    disabled: false,
+    id: "UA-177199673-1",
+    disabled: () => {
+      const getGDPR = window.localStorage.getItem("GDPR:accepted");
+      if (typeof getGDPR !== null && getGDPR === "no") {
+        return true;
+      }
+      if (typeof getGDPR !== null && getGDPR === "yes") {
+        return false;
+      }
+      return true;
+    },
+    router: ctx.app.router,
+    //Remove debug before deployment
     debug: {
-      enabled: true,
-      trace: true,
-      sendHitTask: true
-    }
-  })
-} else if (typeof getGDPR !== 'undefined' && getGDPR === 'false') {
-  Vue.use(vueAnalytics, {
-    id: 'UA-177199673-1',
-    disabled: true,
-    debug: {
-      enabled: true,
-      trace: true,
-      sendHitTask: true
-    }
-  })
-}
+      enabled: false,
+      sendHitTask: false,
+    },
+  });
+};
