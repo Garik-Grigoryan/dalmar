bc7b10d9dd8ffc150b6bfb13dafc33cb438006e5
Edmon Avetyan <edmonavetyan@gmail.com>
2021-04-21 16:00:02 +0400

    corrections

--- components/productDetail.vue
+++ components/productDetail.vue
@@ -92,7 +92,7 @@
       return {
         title: this.product.name_am,
         meta: [
-          { hid: 'Davmar - ' + this.product.name_am, name: 'Davmar ' + this.product.name_am, content: 'Davmar ' + this.product.description_am },
+          { hid: 'Davmar - ' + this.product.name_am, name: 'Davmar ' + this.product.name_am, content: 'Davmar ' + this.product.description_am.replace(/<[^>]*>?/gm, '') },
           {
             hid: "og:image",
             property: "og:image",
@@ -101,7 +101,7 @@
           {
             hid: "description",
             name: "description",
-            content: this.product.description_am,
+            content: this.product.description_am.replace(/<[^>]*>?/gm, ''),
           },
 
         ],
